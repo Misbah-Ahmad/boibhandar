0560d270125ea5221410ae02150964743d1f8952
Misbah Ahmad <imiiucian@gmail.com>
2019-04-10 16:19:34 +0600

    modify users table, add fname, sname, unique phone, address, needs migrate:refresh

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('phone');
+            $table->string('fname');
+            $table->string('sname');            
+            $table->string('phone')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('address')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
