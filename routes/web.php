aed11f8050b7ddf0e7c6b962d31f9dcb15074eb1
Misbah Ahmad <imiiucian@gmail.com>
2019-04-19 23:25:34 +0600

    add book, author show route, wishlist save route

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,11 @@ Route::get('/', function () {
 
 Auth::routes(['verify' => true]);
 
-Route::group(['middleware' => 'verified'], function () {
-    Route::get('/dash', 'HomeController@index')->name('dash');    
+Route::group(['middleware' => ['verified', 'auth']], function () {
+    Route::get('/dash', 'HomeController@index')->name('dash');
+    Route::post('/user/wishlist', 'WishlistController@store')->name('wishlist.store');    
 });
 
+Route::get('/books/{book}', 'BookController@show')->name('books.show');
 
+Route::get('/authors/{author}', 'AuthorController@show')->name('authors.show');
