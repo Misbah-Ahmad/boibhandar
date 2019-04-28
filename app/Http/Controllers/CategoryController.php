a1ac6eff26a084c994de88c64de16878d1848a6c
Misbah Ahmad <imiiucian@gmail.com>
2019-04-28 21:40:22 +0600

    category & publisher books

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Category;
 
 class CategoryController extends Controller
 {
-    //
+    public function show(Category $category)
+    {
+        $books = $category->books;
+
+        return view('categories.show',  compact([
+            'books', 'category',
+
+        ]));
+    }
 }
--- app/Http/Controllers/PublisherController.php
+++ app/Http/Controllers/PublisherController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Publisher;
 use Illuminate\Http\Request;
 
 class PublisherController extends Controller
 {
-    //
+    
+    public function show(Publisher $publisher)
+    {
+        $books = $publisher->books;
+
+        return view('publishers.show',  compact([
+            'books', 'publisher',
+
+        ]));
+    }
+
 }
