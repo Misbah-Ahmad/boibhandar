88ff1997b85064e08a775c4a4a7301f693764cb8
Misbah Ahmad <imiiucian@gmail.com>
2019-06-15 22:34:48 +0600

    price fix

--- resources/views/books/price.blade.php
+++ resources/views/books/price.blade.php
@@ -1,7 +1,7 @@
         Tk.
         @if ($book->hasDiscount && $book->discountedPrice < $book->price)
             
-          <del class="text-muted"> {{ $book->price }}</del> {{ $book->discountedPrice }}
+          <del class="text-muted"> {{ $book->price }}</del> {{ intval($book->discountedPrice) }}
           
         @else
         
