3929a6842e497f88c2c38556b3c762d76a3ecb2c
Misbah Ahmad <imiiucian@gmail.com>
2020-07-12 22:00:38 +0600

    delete all button in cart

--- resources/views/carts/cart.blade.php
+++ resources/views/carts/cart.blade.php
@@ -13,6 +13,15 @@
         
         <div class="col-md-8">    
 
+            <div class="delete-all-from-cart mb-3 bg-white shadow">
+                <div class="d-flex align-items-center justify-content-between p-2">
+                    <h6 class="m-0 text-gray-dark"><small class="">Remove All Items</small></h6>
+                    <form method="POST" action="{{ route('carts.delete_all') }}">
+                        @csrf
+                        <button type="submit" class="btn btn-sm btn-danger">Remove All</button>
+                    </form>
+                </div>
+            </div>
             @foreach ($books as $book)
                 @include('carts.single', ['book' => $book])
             @endforeach
